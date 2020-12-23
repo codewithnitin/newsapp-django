ad6f72986ff9ee14df195198685cbe78348405a9
Nitin Sharma <s19nitin@gmail.com>
2020-12-23 13:13:33 +0530

    Secret-key updated

--- News/settings.py
+++ News/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z9i^_#)nk!pvgvgw$1nvg6%ia*6mt^&o@cb-_-*bdulcq@p2m&'
+SECRET_KEY = 'Your django-key will be here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
